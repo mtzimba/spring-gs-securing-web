917c4d3323a06a14a08d50367f97d33839d77847
Matheus Cardoso <mtzimba@gmail.com>
2023-08-14 11:24:40 -0300

    first commit

---
+++ src/test/java/com/mtzimba/spring/gssecuringweb/GsSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzimba.spring.gssecuringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
